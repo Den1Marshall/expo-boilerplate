eb0296cefc6618d012b4386861d3c1ea5c7385ea
Denys Hrychulevych <111864961+Den1Marshall@users.noreply.github.com>
2025-05-04 18:09:48 +0300

    feat: setup redux toolkit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,12 @@
+import { store } from '@/src/app-root/config';
 import '@/src/app-root/ui/global.css';
 import { Stack } from 'expo-router';
+import { Provider } from 'react-redux';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Provider store={store}>
+      <Stack />
+    </Provider>
+  );
 }
